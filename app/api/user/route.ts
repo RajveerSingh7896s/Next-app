f7ccf84b35fd604f20439e9abe6433344662c86f
Rajveer Singh Chandel <chandelrajveer7896@gmail.com>
2026-03-10 19:52:10 +0530

    used prisma and connected to postgresql db handled api endpoint

---
+++ app/api/user/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest } from "next/server";
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+}); 
+const client = new PrismaClient({
+  adapter,
+});
+export async function POST(req:NextRequest){
+    //extract the body
+    const body = await req.json() ;
+    //store in db
+    await client.user.create({
+        data: {
+            username:body.username,
+            password:body.password
+        }
+    })
+    return Response.json({
+        message:"You are logged in!"
+    })
+}
--- components/SigninComponent.tsx
+++ components/SigninComponent.tsx
@@ -1,9 +1,13 @@
-"use client"
+"use client";
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { ChangeEventHandler, useState } from "react";
 
 export function SigninComponent() {
-  function handler() {
-    console.log("hiii there.");
-  }
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter() ;
   return (
     <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
@@ -17,16 +21,31 @@ export function SigninComponent() {
             </div>
 
             <div className="pt-2">
-              <LabelledInput label="Username" placeholder="abc@gmail.com" />
+              <LabelledInput
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+                label="Username"
+                placeholder="abc@gmail.com"
+              />
 
               <LabelledInput
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
                 label="Password"
                 type={"password"}
                 placeholder="123456"
               />
 
               <button
-                onClick={handler}
+                onClick={async() => {
+                  await axios.post("http://localhost:3000/api/user", {
+                    username,
+                    password,
+                  });
+                  router.push("/");
+                }}
                 type="button"
                 className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
               >
@@ -44,9 +63,15 @@ interface LabelledInputType {
   label: string;
   placeholder: string;
   type?: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
 }
 
-function LabelledInput({ label, placeholder, type }: LabelledInputType) {
+function LabelledInput({
+  label,
+  placeholder,
+  type,
+  onChange,
+}: LabelledInputType) {
   return (
     <div>
       <label className="block mb-2 text-sm text-black font-semibold pt-4">
@@ -54,6 +79,7 @@ function LabelledInput({ label, placeholder, type }: LabelledInputType) {
       </label>
 
       <input
+        onChange={onChange}
         type={type || "text"}
         id="first_name"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
